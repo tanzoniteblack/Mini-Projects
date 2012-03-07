caa3d3bc8af516c8c29df56abd5d43182a7b62f7
Ryan Smith <rnsmith2@gmail.com>
2012-03-06 20:26:40 -0800

    kanjiextract.py is now able to read through the xml file and extract the 2501 most common Kanjis and print them in order.

--- KanjiExtract/kanjiextract.py
+++ KanjiExtract/kanjiextract.py
@@ -1,10 +1,21 @@
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree as etree
 import sys
 
-xml_file = open(sys.argv[1])
+with open("kanjidic2.xml") as xml_file:
+    tree = etree.parse(xml_file)
 
-tree = ET.parse(xml_file)
+most_freq = {}
+root = tree.getroot()
 
-for element in tree.getiterator():
-    print(element)
-    sys.exit()
+for element in root:
+    if element.tag == "character":
+        misc = element.find("misc")
+        freq = misc.find("freq")
+        if freq is not None:
+            most_freq[int(freq.text)] = element
+
+most_freq_nums = sorted(most_freq)
+most_freq = [most_freq[x] for x in most_freq_nums]
+
+for item in most_freq:
+    print(item.find("literal").text, item.find("misc").find("freq").text)
